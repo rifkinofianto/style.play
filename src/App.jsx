676154adcb94a3cafc452d1dfbdb2325c23944b8
rifkinofianto <136487651+rifkinofianto@users.noreply.github.com>
2024-09-12 13:25:12 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Dashboard from './page/Dashboard';
+import Product from './page/Product';
+import LoginPage from './page/LoginPage';
+function App() {
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage/>} />
+        <Route path="/dashboard" element={<Dashboard page = "dashboard"/>} />
+        <Route path='/product' element={<Product page = "product"/>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
